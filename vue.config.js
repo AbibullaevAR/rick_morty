28a09dcbd93b13d22f6b2cad845d61c3c706c294
AbibullaevAR <a.alim2019@mail.ru>
2024-05-29 13:51:42 +0300

    add public path

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: 'https://abibullaevar.github.io/rick_morty/dist/'
 });
